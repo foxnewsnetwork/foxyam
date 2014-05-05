3ab971ee5fce5ca2b3e9b7c74c8fbb6b2c48bc27
Thomas Chen <foxnewsnetwork@gmail.com>
2014-05-05 16:27:47 -0700

    got rid of unnecessary interactors

--- spec/interactors/fox_yam/negotiations/sell_offers/interactor_spec.rb
+++ spec/interactors/fox_yam/negotiations/sell_offers/interactor_spec.rb
@@ -17,13 +17,6 @@ describe FoxYam::Negotiations::SellOffers::Interactor do
   describe '#offer!' do
     specify { expect { result }.to change(FoxYam::Offer, :count).by 1 }
     specify { expect { result }.to change(FoxYam::Conversation, :count).by 1 }
-    specify { expect { result }.to change(FoxYam::Conversations::Price, :count).by 1 }
     specify { expect { result }.to change(FoxYam::Company, :count).by 1 }
-  end
-  context 'relationships' do
-    before { result }
-    specify { interactor.tag.should be_a FoxYam::Conversations::Price }
-    specify { interactor.tag.should be_persisted }
-    specify { interactor.tag.conversation.should eq interactor.conversation }
   end
 end
