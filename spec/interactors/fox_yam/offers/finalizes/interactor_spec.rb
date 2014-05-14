8fb372622d51c327f77ff5c51bd84fe20c82d31c
Thomas Chen <foxnewsnetwork@gmail.com>
2014-05-13 17:49:43 -0700

    fixed occasional test bug

--- spec/interactors/fox_yam/offers/finalizes/interactor_spec.rb
+++ spec/interactors/fox_yam/offers/finalizes/interactor_spec.rb
@@ -4,8 +4,8 @@ describe FoxYam::Offers::Finalizes::Interactor do
   let(:offer) { Factories::Offer.mock }
   let(:raw_interactor) { described_class.new offer }
   let(:interactor) { raw_interactor.tap { |i| i.agreed = true } }
-  let(:result) { interactor.finalizes! }
-  describe '#finalizes!' do
+  let(:result) { interactor.finalize! }
+  describe '#finalize!' do
     it 'should create an line item' do
       expect { result }.to change(FoxYam::LineItem, :count).by 1
     end
--- spec/support/factories/merchant_factory.rb
+++ spec/support/factories/merchant_factory.rb
@@ -1,5 +1,11 @@
 class Factories::Merchant < Factories::Base
+  class << self
+    def count
+      (@count ||= 0) 
+      @count += 1
+    end
+  end
   def attributes
-    { name: Faker::Company.name }
+    { name: [Faker::Company.name, self.class.count].join('-') }
   end
 end
